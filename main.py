6659e10aaabee1d800092e0554ff66cba1d6b776
VH <hirawatt@pm.me>
2023-01-02 20:06:19 +0530

    data reference formatting

--- main.py
+++ main.py
@@ -16,27 +16,7 @@ def main() -> None:
     info = pd.read_csv('./data/info.csv')
     #st.dataframe(info)
 
-    st.write(Path("./data/DATA.md").read_text())
-    st.markdown(
-    """
-    ## Data Sources
-
-    ||||
-    |:-:|:-:|:-:|
-    |NSE Nifty Small Cap 100| [Nifty Small Cap 100](https://www1.nseindia.com/content/indices/ind_niftysmallcap100list.csv)||
-    |All Companies List| [Screener Screens](https://www.screener.in/screens/722948/all-companies-list/)||
-    
-    ## Glossary
-
-    |Emoji|Meaning|
-    |:-:|:-:|
-    |✅|FIXED|
-    |🚀|ADDED|
-    |👍|IMPROVED|
-    |🧪|EXPERIMENT|
-
-    """
-    )
+    st.write(Path("DATA.md").read_text())
     
 if __name__ == "__main__":
     st.set_page_config(
